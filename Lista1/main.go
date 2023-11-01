5f2a6330e0ecebd565121a2c73499f037373f771
szjaniak <szjaniak1@gmail.com>
2023-11-01 21:10:36 +0100

    need to change if statements checking if channels are nil

--- Lista1/main.go
+++ Lista1/main.go
@@ -18,60 +18,57 @@ type vertex struct {
 	channel chan *traveller
 }
 
-func new_vertex(symbol string, channel chan *traveller) {
-	ver := vertex{
-		locator : symbol,
-		channel : channel
-	}
+func new_vertex(symbol string, channel chan *traveller) *vertex{
+	ver := vertex{ locator : symbol, channel : channel }
 
 	return &ver
 }
 
-func get_random_empty_vertex(traveller *traveller) {
-	s1 := rand.NewSource(time.now().UnixNano())
+func get_random_empty_vertex(trav *traveller, board [][]*vertex) {
+	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
 	m1 := r1.Intn(m)
 	n1 := r1.Intn(n)
 
-	if (!board[m1][n1].traveller) 
-		board[m1][n1].traveller <- traveller
-		traveller.pos_x = m1
-		traveller.pos_y = n1
+	if board[m1][n1].channel == nil {
+		board[m1][n1].channel <- trav
+		board[m1][n1].locator = strconv.Itoa(trav.num)
+		trav.pos_x = m1
+		trav.pos_y = n1
 		return
+	}
 
-	get_random_empty_vertex(traveller)
+	get_random_empty_vertex(trav, board)
 }
 
 func vertex_listener(vert *vertex) {
 	for {
 		traveller := <- vert.channel
-		vert.traveller = traveller
 		vert.locator = strconv.Itoa(traveller.num)
 	}
 }
 
 type traveller struct {
-	num int16
-	pos_x int16
-	pos_y int16
+	num int
+	pos_x int
+	pos_y int
 }
 
-func new_traveller(num int) {
-	traveller := traveller{
-		num : num
-	}
+func new_traveller(num int, board [][]*vertex) *traveller{
+	traveller := traveller{ num : num }
 
-	get_random_empty_vertex(traveller)
+	get_random_empty_vertex(&traveller, board)
 
 	return &traveller
 }
 
-func spawn_random_traveller() {
+func spawn_random_traveller(board [][]*vertex) {
 	for{
-		traveller := new_traveller(++k)
-		go run_traveller(traveller)
-		if k >= m * n break
+		traveller := new_traveller(k, board)
+		k++
+		go run_traveller(traveller, board)
+		if k >= m * n { break }
 		time.Sleep(time.Second * 10)
 	}
 }
@@ -82,39 +79,43 @@ func run_traveller(traveller *traveller, board [][]*vertex) {
 		y := traveller.pos_y
 		switch direction := rand.Intn(4); direction {
 		case 0:
-			if x + 1 < m && !board[x + 1][y].traveller
-				board[x + 1][y].channel <= traveller
-				board[x][y].traveller = nil
+			if x + 1 < m && board[x + 1][y].channel == nil{
+				board[x + 1][y].channel <- traveller
+				board[x][y].channel = nil
+			}
 		case 1:
-			if x - 1 >= 0 && !board[x - 1][y].traveller
-				board[x - 1][y].channel <= traveller
-				board[x][y].traveller = nil
+			if x - 1 >= 0 && board[x - 1][y].channel == nil{
+				board[x - 1][y].channel <- traveller
+				board[x][y].channel = nil
+			}
 		case 2:
-			if y + 1 < n && !board[x][y + 1].traveller
-				board[x][y + 1].channel <= traveller
-				board[x][y].traveller = nil
+			if y + 1 < n && board[x][y + 1].channel == nil{
+				board[x][y + 1].channel <- traveller
+				board[x][y].channel = nil
+			}
 		case 3:
-			if y >= 0 && !board[x][y - 1].traveller
-				board[x][y - 1].channel <= traveller
-				board[x][y].traveller = nil
+			if y >= 0 && board[x][y - 1].channel == nil{
+				board[x][y - 1].channel <- traveller
+				board[x][y].channel = nil
+			}
 
 		duration := rand.Intn(500)
-		time.Sleep(time.Duration(duration) * time.Milisecond)
+		time.Sleep(time.Duration(duration) * time.Millisecond)
 		}
 	}
 }
 
-func print_board(board [][]int) {
+func print_board(board [][]*vertex) {
 	var i int
 	var j int
 
 	for {
 		for i = 0; i < m; i++ {
 			for j = 0; j < n; j++ {
-				if board[i][j] == 0 {
+				if board[i][j].channel == nil {
 					fmt.Printf("|--|")
 				} else {
-					fmt.Printf("|%d|", board[i][j])
+					fmt.Printf(board[i][j].locator)
 				}
 			}
 			fmt.Println()
@@ -126,7 +127,7 @@ func print_board(board [][]int) {
 
 func main() {
 	argsWithoutProg := os.Args[1:]
-	k = 0
+	k = 1
 	m, err = strconv.Atoi(argsWithoutProg[0])
 	n, err = strconv.Atoi(argsWithoutProg[1])
 
@@ -147,10 +148,8 @@ func main() {
 		board = append(board, vertex_row)
 	}
 
-	go spawn_random_traveller()
+	go spawn_random_traveller(board)
 	go print_board(board)
-	time.Sleep(time.Second* 5)
-	board[2][2] = 45
-	board[1][1] = 12
-	time.Sleep(time.Second * 10)
+
+	select {}
 }
